bd2fed9b61bf6e93e40bdb86c6f115fde59007d9
John Wayne <ionutpopovici92@gmail.com>
2017-09-17 20:47:04 +0300

    Added Cave of Programming folder and Getting Started folder

---
+++ Cave_of_Programming/Getting_Started/OutputtingText/OutputtingText/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << "Starting program ... " << flush;
+	cout << "This is some text." << endl;
+	cout << "Apple." << " Banana." << " Orange." << endl;
+	cout << "This is some text." << endl;
+
+	system("pause");
+	return 0;
+}
